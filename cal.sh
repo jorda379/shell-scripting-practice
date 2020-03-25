29a6f7ecb91f3a05c90545d688c4131c0b5f6a00
jorda379 <60120307+jorda379@users.noreply.github.com>
2020-03-25 18:02:40 +0100

    Add introductory message: fix typo in instructions

--- cal.sh
+++ cal.sh
@@ -1,1 +1,14 @@
-echo "Hello, welcome to the caluator program"
+#! /usr/bin/env bash
+
+# When the program is first loaded, display a greeting to the user.
+# Then, display a menu that outlines the possible operations:
+  # Add
+  # Subtract
+  # Exit
+# Then, capture the user selection.
+# If the selection matches a supported operation, execute the operation.
+# If the selection does not match a supported operation, display an error message.
+# When the operation is complete, redisplay the menu.
+
+echo "Hello, welcome to the calculator program"
+
