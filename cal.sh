ce75f47b8461bf6d17de2a1163bb109f9360b8d2
jorda379 <60120307+jorda379@users.noreply.github.com>
2020-03-25 18:35:38 +0100

    Display menu of operations

--- cal.sh
+++ cal.sh
@@ -1,14 +1,21 @@
 #! /usr/bin/env bash
 
-# When the program is first loaded, display a greeting to the user.
+# When the program is first loaded, display a greeting to the user
+echo "Hello, welcome to the calculator program"
+
 # Then, display a menu that outlines the possible operations:
   # Add
   # Subtract
   # Exit
+echo "Please make a selection:"
+echo "Add"
+echo "Subtract"
+echo "Exit"
+
+
+
 # Then, capture the user selection.
+
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a supported operation, display an error message.
 # When the operation is complete, redisplay the menu.
-
-echo "Hello, welcome to the calculator program"
-
