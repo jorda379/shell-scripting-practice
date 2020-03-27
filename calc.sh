09ebf8cabc367924b114d9b105aed73cd5cf9a7c
jorda379 <60120307+jorda379@users.noreply.github.com>
2020-03-27 21:40:26 +0100

    Implement loop and 'Exit' operation

--- calc.sh
+++ calc.sh
@@ -4,21 +4,22 @@
 # When the program is first loaded, display a greeting to the user.
 echo "Hello, welcome to the calculator program."
 
-# Then, display a menu that outlines the possible operations:
-  # Add
-  # Subtract
-  # Exit
-echo "Please make a selection:"
-echo "Add"
-echo "Subtract"
-echo "Exit"
+ # Keep prompting the user for input until they exit the program.
+while true; do
+  # Then, display a menu that outlines the possible operations:
+   # Add
+   # Subtract
+   # Exit
+   echo "Please make a selection:"
+   echo "Add"
+   echo "Subtract"
+   echo "Exit"
 
+   # Then, capture the user selection.
+   read selection
 
-# Then, capture the user selection.
-read selection
-
-# If the selection matches a supported operation, execute the operation.
-case $selection in 
+   # If the selection matches a supported operation, execute the operation.
+   case $selection in 
    "Add")
     echo "Please provide the first number:"
     read first_number
@@ -35,19 +36,17 @@ case $selection in
     echo "The difference of $first_number and $second_number is:"
     expr $first_number -  $second_number
     ;;
-    "Exit")
-     echo "User has selected 'Exit'"
-    ;;  
-  # If the selection does not match a supported operation, display an error message.
-  *)
-   echo "Unsupported operation: $selection"
-   echo "Please try again"
-esac
-
-# When the operation is complete, redisplay the menu.
-
-
-
+   "Exit")
+     echo "Come again soon. Caio caio."
+     exit 0
+     ;;  
+   # If the selection does not match a supported operation, display an error message.
+   *)
+     echo "Unsupported operation: $selection"
+     echo "Please try again"
+     ;;
+    esac
+    done
 
 
 
@@ -55,3 +54,4 @@ esac
 
 
 
+ 
